50cc7dfa9d282c2de01961998bf1dddde4143b9a
zhangzhiqiangit <zhangzhiqiang_it@outlook.com>
2016-02-15 18:29:52 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,7 +78,7 @@
 						$('.main.style1')
 							.scrollex({
 								mode:		'middle',
-								delay:		100,
+								delay:		50,
 								initialize:	function() { $(this).addClass('inactive'); },
 								terminate:	function() { $(this).removeClass('inactive'); },
 								enter:		function() { $(this).removeClass('inactive'); },
@@ -88,7 +88,7 @@
 						$('.main.style2')
 							.scrollex({
 								mode:		'middle',
-								delay:		100,
+								delay:		50,
 								initialize:	function() { $(this).addClass('inactive'); },
 								terminate:	function() { $(this).removeClass('inactive'); },
 								enter:		function() { $(this).removeClass('inactive'); },
