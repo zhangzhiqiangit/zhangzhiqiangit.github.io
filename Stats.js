4db6df68bdb5da89a8aeab6629e2a1b3cfffa409
zhangzhiqiangit <zhangzhiqiang_it@outlook.com>
2016-07-22 09:47:55 +0800

    v2.0

--- Stats.js
+++
@@ -1,6 +0,0 @@
-var count_particles, stats, update;
-count_particles = document.querySelector('.js-count-particles');
-update = function() {    
-    requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
